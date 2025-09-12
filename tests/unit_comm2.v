e4f710d876a8cd9d49d0902ff5f5990bf029fad9
Mysaa Java <mysaa@hadoly.fr>
2025-09-12 21:39:05 +0200

    Rewrite of Trocq with a dedicated `aterm` datatype
    Here is a summary of all the changes i made for this PR. Please note that the example suite and the test suite still work.
    
    - Push nixpkgs commit hash (i needed some elpi bugs have been fixed)
    - Now runs the test suite when building the Nix derivation trocq-std and trocq-hott
    - Fix Makefile bug for tests, the symlink _CoqProject was not removed
    - 3 new tests, 2 of them failing and therefore disabled
    
    - Unified logging format with "[function-name] blah blah blah"
    - Made predicates from `class.elpi` functional
    - Moved all code out of `database.elpi`, only to keep a clean 'header' of the database. Uvar-checking and gref-to-term content has been moved to namespace `trocq.p` in file `param-class-util`.  Removed coq.univ-instance uses, replaced with `coq.env.global`.
    - We register the weakening coercions from a map-class to a map-class within distance of 1 to the database `trocq.db.forget`.
    - Database accessors are now in a more friendly form `trocq.p.*` for * being `univalence`, `funext`, `ttype`, `arrow`, `forall`, `r`, `forget`. Those predicates directly return terms instead of grefs or consts, and they are suspended when given an uvar.
    - Moved the logic of 'checking wether any axiom is needed' to `trocq.p` so it does not pollute `param` predicate
    - Some α-renaming has been done, along with my rewriting of trocq rules. Every name should be coherent once i would have rewrote the trocq rules entirely.
    - Rewrote `classes` predicate so there is only one induction
    - `util.option.value` now suspended on an uvar
    - `util.mk-app` creates an application node without doing any β-reduction. Both [coq.mk-app](https://github.com/LPCIC/coq-elpi/blob/af6a8f45eb0da7cc6790c13250beb1f2800fa5c3/elpi/coq-lib.elpi#L69) and [coq.subst-fun](https://github.com/LPCIC/coq-elpi/blob/af6a8f45eb0da7cc6790c13250beb1f2800fa5c3/elpi/coq-lib.elpi#L32) applies β-reduction, which can lead to issue https://github.com/LPCIC/coq-elpi/issues/941
    
    - Creating `aterm`, described in file `aterm.elpi`. It stands for 'annotated type' and they replace 'PType' (and 'PProp') which were previously used. They appear in the `param` predicate as first and second element, and in every place where one expected an annotated term.
    - No 'Weaken' placeholders anymore, weakening is done in place, and we β-reduce the proof after the resolution of parametricity classes. This is possible as Weaken is no longer a meta-term function, and is directly encoded as a term by `trocq.p.forget` which comes as a replacement for `param-class.forget`. Therefore the predicate `weakening` that would send back the meta-term function has been replaced with a `weakener` predicate that sends back the weakener as a term.
    - Removed `univ-link` when adding nodes to the graph. This was used as a 'layer' of uvar between the parametricity classes and the graph nodes, which is unnecessary with the new rule 'do not unfold param-class unless you instantiate'.
    - Added some TODOs related to broken examples.
    
    I established some rules to make further development of the codebase easier:
    - Never have a term of type term `pglobal G _` or `global G` if G is an uvar (it makes Rocq unable to print it)
    - Never have a term of type param-class `pc M N` if M or N is an uvar (it allows much shorter code where we only have to check U = uvar, rather than U = uvar | U = pc uvar _ | U = pc _ uvar | U = pc uvar uvar)
    - Predicates `sub-type`,`eq`,`classes` should only be called on aterms that are types, i.e. aterms whose forgotten term has type some sort. This allows to remove edge cases that are not well defined (for exemple, such a term cannot be λx:_,_). There is still some code used internally by those functions (i.e. recursive calls), but this should be removed.
    - Logging prints should start with a tag '[f]' where f is the calling function

---
+++ tests/unit_comm2.v
@@ -0,0 +1,69 @@
+(*****************************************************************************)
+(*                            *                    Trocq                     *)
+(*  _______                   *       Copyright (C) 2023 Inria & MERCE       *)
+(* |__   __|                  *    (Mitsubishi Electric R&D Centre Europe)   *)
+(*    | |_ __ ___   ___ __ _  *       Cyril Cohen <cyril.cohen@inria.fr>     *)
+(*    | | '__/ _ \ / __/ _` | *       Enzo Crance <enzo.crance@inria.fr>     *)
+(*    | | | | (_) | (_| (_| | *   Assia Mahboubi <assia.mahboubi@inria.fr>   *)
+(*    |_|_|  \___/ \___\__, | ************************************************)
+(*                        | | * This file is distributed under the terms of  *)
+(*                        |_| * GNU Lesser General Public License Version 3  *)
+(*                            * see LICENSE file for the text of the license *)
+(*****************************************************************************)
+
+From Trocq Require Import Stdlib Trocq.
+
+Set Universe Polymorphism.
+
+Section N.
+
+    Trocq Logging trace.
+    Inductive N : Set :=
+    | Z : N
+    | S : N -> N.
+    Fixpoint ad (n m : N) : N :=
+        match n with
+        | Z => m
+        | S p => S (ad p m)
+        end.
+
+    Definition NR : Param44.Rel N N. admit. Admitted.
+    Trocq Use NR.
+    Definition NS {n n': N} (Rr : NR n n'): NR (S n) (S n'). admit. Admitted.
+    Trocq Use NS.
+    Definition Nad {n n' m m' : N} (Rr : NR n n') (Rr' : NR m m'): NR (ad n m) (ad n' m'). admit. Admitted.
+    Trocq Use Nad. 
+
+    Variable A : Type.
+
+    Variable (T : N -> Type) (T' : N -> Type).
+    Variable (append : forall {n m : N}, T n -> T m -> T (ad n m)).
+    Variable (append' : forall {n m : N}, T' n -> T' m -> T' (ad n m)).
+
+    Definition NT
+{n n' : N} (nR : NR n n'): Param2a2b.Rel (T n) (T' n'). admit. Admitted.
+Trocq Use NT.
+
+
+Definition Nappend {n n' : N} (nR : NR n n')
+    {m m' : N} (mR : NR m m')
+    {t : T n} {t' : T' n'} (tR : NT nR t t')
+    {u : T m} {u' : T' m'} (uR : NT mR u u') :
+    NT (Nad nR mR) (append t u) (append' t' u'). admit. Admitted. 
+Trocq Use Nappend.
+
+    Variable (P : forall {n : N}, T n -> Type).
+    Variable (P' : forall {n : N}, T' n -> Type).
+    Definition RP {n n' : N} (nR : NR n n')
+        {t : T n} {t' : T' n'} {tR : NT nR t t'}
+        : Param44.Rel (P t) (P' t').
+        admit. Admitted.
+    Trocq Use RP.
+
+    Goal forall {n1 n2 : N}
+        (v1 : T n1) (v2 : T n2),
+        P (append v1 v2).
+    trocq.
+    Abort.
+
+End N.
