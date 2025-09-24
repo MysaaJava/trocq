b91422751a28e8450f63441451e865fd509ac326
Mysaa Java <mysaa@hadoly.fr>
2025-09-24 19:57:52 +0200

    unit_transfer fails again (that was needed)

--- tests/unit_transfer.v
+++ tests/unit_transfer.v
@@ -16,26 +16,67 @@ From Trocq Require Import Stdlib Trocq.
 Set Universe Polymorphism.
 
 Section Transfer.
-    Variable (I I' : Type) (f : I' -> I).
-    Variable (p : I -> I -> I) (p' : I' -> I' -> I').
+
+    Trocq Logging trace.
+
+    Variable (I I' : Type) (f : I' -> I) (f' : I -> I').
 
     Definition Rf := mkParam2a0 f.
     Trocq Use Rf.
+    Definition Rf' := mkParam2a0 f'.
+    Trocq Use Rf'.
+
+    Variable (pe : I -> I -> Prop) (pe' : I' -> I' -> Prop).
+    Definition Rpe (m : I) (m' : I') (rm : (Rf') m m')
+        (n : I) (n' : I') (rn : (Rf') n n')
+        : Param01.Rel (pe n m) (pe' n' m').
+        admit.
+    Admitted.
+    Trocq Use Rpe.
+
+    Goal forall (m : I), pe m m.
+    Proof.
+    trocq.
+    enough (x : forall m' : I', pe' m' m') by exact x.
+    Abort.
+
+    (*
+    Variable (I I' : Type) (f : I' -> I) (f' : I -> I').
+    Variable (p : I -> I -> I) (p' : I' -> I' -> I').
+    Variable (pe : I -> I -> Prop) (pe' : I' -> I' -> Prop).
+
+    Definition Rf := mkParam2a0 f.
+    Trocq Use Rf.
+    Definition Rf' := mkParam2a0 f'.
+    Trocq Use Rf'.
 
     Definition Rg (m : I) (m' : I') (rm : (Rf) m' m)
         (n : I) (n' : I') (rn : (Rf) n' n)
         : (Rf) (p' n' m') (p n m).
         admit.
     Admitted.
-
     Trocq Use Rg.
 
+    Definition Rp (m : I) (m' : I') (rm : (Rf) m' m)
+        (n : I) (n' : I') (rn : (Rf) n' n)
+        : Param00.Rel (pe' n' m') (pe n m).
+        admit.
+    Admitted.
+    Trocq Use Rp.
+
     Trocq Logging trace.
+
+    
+    Goal forall m : I', pe' m m.
+        trocq.
+        admit.
+    Admitted.
+    
 
     Goal forall m: I', @eq I' m (p' m m).
         trocq.
         enough (forall m n : I, m = p n n -> m = n) by exact x.
     Abort.
-
+    *)
 
 End Transfer.
